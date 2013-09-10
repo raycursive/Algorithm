33206f6d153b116a9f480e967f13fc275b799b5a
RayZhang <m@raycursive.com>
2013-09-10 18:03:46 +0800

    Update Prim.py

--- Prim.py
+++ Prim.py
@@ -13,39 +13,36 @@ import heapq
 #Vertices:{vertex}
 
 
-def prim(V,E,start=1):
-        X={start}
-        T={}
+def prim(V,E,start = 1):
+        X = {start}
+        T = {}
         key = {i:(999999,(0,0)) for i in V}
         for i in E:
-                u,v=i
-                value=E[i]
-                tmpweight,tmpedge=key[u]
-                if u==start and tmpweight > value:
+                u, v = i
+                value = E[i]
+                tmpweight, tmpedge = key[u]
+                if u == start and tmpweight > value:
                         key[v] = (value,i)
-                tmpweight,tmpedge=key[v]
-                if v==start and tmpweight > value:
+                tmpweight, tmpedge = key[v]
+                if v == start and tmpweight > value:
                         key[u] = (value,i)
-        heap=list(key.values())
+        heap = list(key.values())
         heapq.heapify(heap)
-        while X!=V:
+        while X != V:
                 value_to_append, edge = heapq.heappop(heap)
                 u, v = edge
-                if u in X:
-                        v_add=v
-                else:
-                        v_add=u
+                v_add = u if v in X else v
                 if not v_add in X:
                         for i in V:
-                                value,tmp=key[i]
-                                if (v_add,i) in E and not(i in X):
-                                        if E[(v_add,i)]<value:
-                                                heapq.heappush(heap,(E[(v_add,i)],(v_add,i)))
-                                if (i,v_add) in E and not(i in X):
-                                        if E[(i,v_add)]<value:
-                                                heapq.heappush(heap,(E[(i,v_add)],(i,v_add)))
+                                value=key[i][0]
+                                if (v_add, i) in E and not(i in X):
+                                        if E[(v_add,i)] < value:
+                                                heapq.heappush(heap,(E[(v_add,i)], (v_add,i)))
+                                if (i, v_add) in E and not(i in X):
+                                        if E[(i,v_add)] < value:
+                                                heapq.heappush(heap,(E[(i,v_add)], (i,v_add)))
                         X.add(v_add)
-                        T[edge]=value_to_append
+                        T[edge] = value_to_append
         return T
 
 print(sum(prim(V,E).values()))
